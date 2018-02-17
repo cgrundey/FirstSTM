af2e347af3a797a5bc331a44c852a667cd9dbf02
cboogs95 <c95grundey@gmail.com>
2018-02-16 20:53:22 -0500

    Code compiles but have seg fault

--- grundeyHW3.cpp
+++ grundeyHW3.cpp
@@ -1,5 +1,14 @@
 /**
 * Software Transactional Memory
+*   The five functions of software transactional memory are implemented
+*   and used in a Bank Account example. Lazy implementation is used so
+*   write-back occurs in tx_commit() and a redo-log is kept of read and
+*   write values to try again. tx_abort() is not explicitly implemented,
+*   rather it is the return value of tx_commit() to show that it was aborted.
+*
+* Author: Colin Grundey
+* Date: February 19, 2018
+*
 * Compile:
 *   g++ grundeyHW3.cpp -o HW3 -lpthread
 */
@@ -32,103 +41,86 @@
 
 using namespace std;
 
+typedef struct {
+  int addr;
+  int value;
+  int ver;
+} Acct;
 
+vector<Acct> accts;
+// unordered_map<pthread_mutex_t, int> myLocks;
+pthread_mutex_t myLocks[NUM_ACCTS];
+int numThreads;
+
+/* Where a transaction begins. Read and write sets are intialized and cleared. */
 void tx_begin(list<Acct>& read_set, list<Acct>& write_set) {
-  // Reset read_set & write_set
-  for (int i = 0; i < 10; i++) {
-    read_set.clear();
-    write_set.clear();
-  }
+  read_set.clear();
+  write_set.clear();
 }
+/* Adds account with version to read_set and returns the value for later use. */
 int tx_read(int addr, list<Acct>& read_set) {
-  /**
-  * lock = read(lock[addr]);
-  * ver = read(version[addr]);
-  * if (!lock) {
-  *   add(ver, read_set);
-  *   return *addr;
-  * }
-  * else
-  *   tx_abort();
-  */
-  int lock = myLocks[addr].lock();
-  if (!lock) {
+  int lck = pthread_mutex_lock(&(myLocks[addr]));
+  if (!lck) {
     read_set.push_back(accts[addr]);
     return accts[addr].value;
   }
   else
-    tx_abort(); // TODO
+    return -1; // ABORT - TODO: maybe consider retrying lock
 }
-
-void tx_write(int addr, int val, list<Acct>& write_set) {
-  /**
-  * lock = read(lock[addr]);
-  * ver = read(version[addr]);
-  * if (!lock)
-  *   write_set.add(addr, value, ver);
-  * else
-  *   tx_abort();
-  */
-  int lock = myLocks[addr].lock();
-  if (!lock) {
+/* Adds a version of the account at addr to write_set with the given value. */
+bool tx_write(int addr, int val, list<Acct>& write_set) {
+  int lck = pthread_mutex_lock(&(myLocks[addr]));
+  if (!lck) {
     Acct temp; temp.addr = addr;
     temp.value = val;
     temp.ver = accts[addr].ver;
     write_set.push_back(temp);
+    return true;
   }
-  else
-    tx_abort(); // TODO
+  return false; // ABORT - TODO: maybe consider retrying lock
 }
-
-bool tx_abort() { // TODO
-  return false;
-}
-
-void tx_commit() {
-
-  // validate
-  // acquire myLocks
-  for (std::list<int>::const_iterator iterator = write_set.begin(), end = write_set.end(); iterator != end; ++iterator) {
-      myLocks[*iterator.addr].lock();
-      if (*iterator.ver != accts[*iterator.addr].ver) {
-        unlock(all);  // TODO
-        tx_abort();   // TODO
-      }
-  }
-  for (std::list<int>::const_iterator iterator = read_set.begin(), end = read_set.end(); iterator != end; ++iterator) {
-      myLocks[*iterator.addr].lock();
-      if (*iterator.ver != accts[*iterator.addr].ver) {
-        unlock(all); // TODO
-        tx_abort();  // TODO
-      }
-  }
-  for (std::list<int>::const_iterator iterator = write_set.begin(), end = write_set.end(); iterator != end; ++iterator) {
-    accts[*iterator.addr] = *iterator.value;
-    accts[*iterator.addr].ver = *iterator.ver;
-    myLocks[*iterator.addr].unlock();
-  }
-
-  /*
-  for each(entry in write_set) {
-    lock(entry);
-    if (entry.version != mem_ver) {
-      unlock(all);
-      tx_abort();
+/* Attempts to commit the transaction. Checks read and write set versions
+ * and compares it to memory versions (i.e. accts[].ver). If valid, all
+ * accounts in read and write sets are written back to memory. */
+bool tx_commit(list<Acct>& read_set, list<Acct>& write_set) {
+  list<Acct>::iterator iterator;
+  bool aborting = false;
+  /* Validate write_set */
+  for (iterator = write_set.begin(); iterator != write_set.end(); ++iterator) {
+    if (aborting) { // Abort_2
+      pthread_mutex_unlock(&(myLocks[iterator->addr]));
+      continue;
+    }
+    pthread_mutex_lock(&(myLocks[iterator->addr]));
+    if (iterator->ver != accts[iterator->addr].ver) {
+      aborting = true; // Abort_1
+      iterator = write_set.begin(); // Begin parse again to unlock all
     }
   }
-  for each(entry in read_set) {
-    if (entry.version != mem_ver) {
-      unlock(all);
-      tx_abort();
+  if (aborting) // Abort_3
+    return false;
+  aborting = false;
+  /* Validate read_set */
+  for (iterator = read_set.begin(); iterator != read_set.end(); ++iterator) {
+    if (aborting) { // Abort_2
+      pthread_mutex_unlock(&(myLocks[iterator->addr]));
+      continue;
+    }
+    pthread_mutex_lock(&(myLocks[iterator->addr]));
+    if (iterator->ver != accts[iterator->addr].ver) {
+      aborting = true; // Abort_1
+      iterator = read_set.begin(); // Begin parse again to unlock all
     }
   }
-  // Validation is a success
-  for each(entry in write_set) {
-    entry_add = entry.value;
-    entry.ver += 1;
-    entry.lock = 0;
+  if (aborting) // Abort_3
+    return false;
+  /* Validation is a success */
+  for (iterator = write_set.begin(); iterator != write_set.end(); ++iterator) {
+    accts[iterator->addr].value = iterator->value;
+    accts[iterator->addr].ver = iterator->ver;
+    pthread_mutex_unlock(&(myLocks[iterator->addr]));
   }
-  */
+  return true;
 }
 
 inline unsigned long long get_real_time() {
@@ -146,16 +138,7 @@ void barrier(int which) {
     CFENCE;
 }
 
-typedef struct {
-  int addr;
-  int value;
-  int ver;
-} Acct;
-
-vector<Acct> accts;
-// unordered_map<pthread_mutex_t, int> myLocks;
-vector<pthread_mutex_t> myLocks;
-
+// Thread function
 void* th_run(void * args)
 {
   long id = (long)args;
@@ -167,7 +150,8 @@ void* th_run(void * args)
 
   int workload = NUM_TRFR/numThreads;
   for (int i = 0; i < workload; i++) {
-    do { // ________________BEGIN_________________
+// ________________BEGIN_________________
+    do {
       tx_begin(read_set, write_set);
       int r1 = 0;
       int r2 = 0;
@@ -178,11 +162,13 @@ void* th_run(void * args)
         }
         // Perform the transfer
         int a1 = tx_read(r1, read_set);
+        if (a1 < 0) {continue;}
         int a2 = tx_read(r2, read_set);
         tx_write(r1, a1 - TRFR_AMT, write_set);
         tx_write(r2, a2 + TRFR_AMT, write_set);
       }
-    } while (!tx_commit()); // ______________END__________________
+    } while (!tx_commit(read_set, write_set));
+// _________________END__________________
   }
   return 0;
 }
@@ -200,14 +186,14 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  // initializing 1,000,000 accounts with $1000 each
+  // Initializing 1,000,000 accounts with $1000 each
   for (int i = 0; i < NUM_ACCTS; i++) {
-    accts[i] = INIT_BALANCE;
+    accts[i].value = INIT_BALANCE;
   }
 
-  // initialize mutex locks
+  // Initialize mutex locks
   for (int i = 0; i < NUM_ACCTS; i++) {
-    if (pthread_mutex_init(&mylocks[i], NULL) != 0) {
+    if (pthread_mutex_init(&myLocks[i], NULL) != 0) {
       printf("\n mutex %d init failed\n", i);
       return 1;
     }
@@ -229,19 +215,21 @@ int main(int argc, char* argv[]) {
 
   long totalMoneyBefore = 0;
   for (int i = 0; i < NUM_ACCTS; i++) {
-    totalMoneyBefore += accts[i].getBalance();
+    totalMoneyBefore += accts[i].value;
   }
 
   for (int i=0; i<ids-1; i++) {
     pthread_join(client_th[i], NULL);
   }
 /* EXECUTION END */
-  pthread_mutex_destroy(&mylock);
+  for (int i = 0; i < NUM_ACCTS; i++) {
+    pthread_mutex_destroy(&myLocks[i]);
+  }
 
-  printf("Total transfers: %d\n", actual_transfers);
+  //printf("Total transfers: %d\n", actual_transfers);
   printf("Total time = %lld ns\n", get_real_time() - start);
   printf("Total Money Before: $%ld\n", totalMoneyBefore);
-  printf("Total Money After: $%ld\n", totalMoneyAfter);
+  // printf("Total Money After: $%ld\n", totalMoneyAfter);
 
   return 0;
 }
